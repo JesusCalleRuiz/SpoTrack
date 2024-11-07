0b06711f7580cfc1def5c08746cae1766ba357e6
jesus.calle <jesus.calle@securitasdirect.es>
2024-11-07 16:09:15 +0100

    correcion errores possibly undefined

--- src/views/MyRoutePage.vue
+++ src/views/MyRoutePage.vue
@@ -88,14 +88,15 @@ const initMap = () => {
       }
     });
     map2.fitBounds(bounds, { padding: 50 });
+
     const start = routeData.value?.path[0];
     new mapboxgl.Marker({ color: "#04ff00" })
-        .setLngLat([start.lng, start.lat])
+        .setLngLat([start?.lng, start?.lat])
         .addTo(map2);
 
     const end = routeData.value?.path[routeData.value?.path.length - 1];
     new mapboxgl.Marker({ color: "#000000" })
-        .setLngLat([end.lng, end.lat])
+        .setLngLat([end?.lng, end?.lat])
         .addTo(map2);
   });
 };
